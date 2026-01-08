521f6afcfa80b10773cb1903082fb0d4dbfa3991
Abrahansito <huamancarlosabraham@gmail.com>
2026-01-08 18:56:09 -0500

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: '*', // para pruebas (luego se restringe)
+    methods: 'GET,POST,PUT,PATCH,DELETE',
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
